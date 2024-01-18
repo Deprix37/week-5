0bbfaf1a4b8a605f7f08ade93bf6ea55e4e9358d
Ahmet Elbüz <elbuzahmet@gmail.com>
2024-01-18 18:35:40 +0300

    Patika Store

---
+++ PatikaStore/src/Operation.java
@@ -0,0 +1,9 @@
+public interface Operation {
+    void runNenu();
+    void showProductList();
+    void addProduct();
+    void deleteProduct();
+    void filterByProductId();
+    void filterByProductBrand();
+
+}
